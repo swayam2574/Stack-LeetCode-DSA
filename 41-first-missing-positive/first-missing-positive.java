fde3ae2b492805f5b5a4bccc5f54a6f66beabfc4
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-03-07 18:20:42 +0530

    Time: 1 ms (99.75%) | Memory: 71.4 MB (46.99%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,39 +1,28 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int n = nums.length;
-        for(int i=0;i<n;i++) {
-            if(nums[i] < 0) nums[i] = 0;
-            max = Math.max(max, nums[i]);
-        }
+    public int firstMissingPositive(int[] nums){
+        int n=nums.length;
+        for(int i=0;i<n;i++) 
+            if(nums[i]<=0) 
+                nums[i]=n+1;
 
         for(int i=0;i<n;i++) {
-            int index = Math.abs(nums[i]) - 1;
-            if(index >= 0 && index < n) {
-                int val = nums[index];
-                if(val == 0) {
-                    nums[index] = (n+1)* - 1;
-                } else {
-                    nums[index] = Math.abs(nums[index]) * - 1;
-                }
+            int p = Math.abs(nums[i]);
+            if(p<=n && nums[p-1]>0){
+                nums[p-1]=-nums[p-1];
             }
         }
-        for(int i=1;i<=n;i++) {
-            int index = i - 1;
-            if(nums[index] >= 0) {
-                return i;
-            }
-        }
-        return max + 1;
+
+        for(int i=0;i<n;i++) 
+            if(nums[i]>0) 
+                return i+1;
+
+        return n+1;
     }
 }
-
-
 
 // fine upto 172 test cases
 // class Solution {
 //     public int firstMissingPositive(int[] nums) {
-//         int min = Integer.MAX_VALUE;
 //         int max = Integer.MIN_VALUE;
 
 //         List<Integer> lp = new ArrayList<>();
@@ -43,7 +32,6 @@ class Solution {
 //             if(i<1) ln.add(i);
 //             else{
 //                 lp.add(i);
-//                 min = Math.min(min, i);
 //                 max = Math.max(max, i);
 //             }
 //         }
