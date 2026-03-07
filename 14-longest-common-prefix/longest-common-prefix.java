fa6b5b802b0cf42607ff91fdaae2b432f247dc4a
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-03-07 18:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (82.80%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -2,39 +2,39 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
 
         // ________________________ method 1
-        if (strs == null || strs.length == 0) return "";
+        // if (strs == null || strs.length == 0) return "";
 
-        Arrays.sort(strs);
-        StringBuilder sb = new StringBuilder();
+        // Arrays.sort(strs);
+        // StringBuilder sb = new StringBuilder();
 
-        String f = strs[0];
-        String l = strs[strs.length-1];
+        // String f = strs[0];
+        // String l = strs[strs.length-1];
 
-        int fl = f.length();
-        for(int i=0; i<fl; i++){
-            if(f.charAt(i)!=l.charAt(i)) break; 
-            sb.append(f.charAt(i));
-        }
+        // int fl = f.length();
+        // for(int i=0; i<fl; i++){
+        //     if(f.charAt(i)!=l.charAt(i)) break; 
+        //     sb.append(f.charAt(i));
+        // }
 
-        return sb.toString();
+        // return sb.toString();
 
 
 
 
 
         // ________________________ method 2
-        // if (strs == null || strs.length == 0) return "";
+        if (strs == null || strs.length == 0) return "";
 
-        // String prefix = strs[0];
+        String prefix = strs[0];
 
-        // for (int i = 1; i < strs.length; i++) {
-        //     while (strs[i].indexOf(prefix) != 0) {
-        //         prefix = prefix.substring(0, prefix.length() - 1);
-        //         if (prefix.isEmpty()) return "";
-        //     }
-        // }
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
 
-        // return prefix;
+        return prefix;
 
     }
 }
