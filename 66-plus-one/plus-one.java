a45275e58ad181877354b1be921d6d80e0ee1bc7
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-03-07 16:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (97.66%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,45 +1,15 @@
-import java.math.BigInteger;
-
 class Solution {
     public int[] plusOne(int[] digits) {
-        // _________________________ m1 
-        // for(int i=digits.length-1; i>=0; i--){
-        //     if(digits[i]<9){
-        //         digits[i]++;
-        //         return digits;
-        //     }
-        //     digits[i]=0;
-        // }
-
-        // int[] res = new int[digits.length+1];
-        // res[0] = 1;
-        // return res;
-
-
-        // _________________________ m2 
-        // // fine only for smaller number
-        // long n = 0;
-        // int p = digits.length-1;
-        // for(int i: digits){
-        //     n+=i*Math.pow(10, p--); 
-        // } 
-
-        StringBuilder num = new StringBuilder();
-
-        for(int i : digits){
-            num.append(i);
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
         }
 
-        BigInteger n = new BigInteger(num.toString());
-        n = n.add(BigInteger.ONE);
-
-        String s = n.toString();
-
-        int[] res = new int[s.length()];
-        for(int i = 0; i < s.length(); i++){
-            res[i] = s.charAt(i) - '0';
-        }
-
+        int[] res = new int[digits.length+1];
+        res[0] = 1;
         return res;
     }
 }
