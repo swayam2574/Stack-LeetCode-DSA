9ee3956622acf588a363d421bd7ef019428afdf4
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-03-30 19:24:07 +0530

    Time: 2 ms (87.23%) | Memory: 42.7 MB (72.73%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] freq = new int[26];
+        char[] ta = tiles.toCharArray();
+        for (char c : ta) {
+            freq[c - 'A']++;
+        }
+        return dfs(freq);
+    }
+
+    private int dfs(int[] freq) {
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) continue;
+            count++;
+            freq[i]--;
+            count += dfs(freq);
+            freq[i]++;
+        }
+        return count;
+    }
+}
